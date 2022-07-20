284891525ea1cf8ad3acca846aab50b93249343a
jayshreevinoth <45241188+jayshreevinoth@users.noreply.github.com>
2022-07-21 00:42:38 +0200

    Add files via upload

--- Higher_order_function.js
+++ Higher_order_function.js
@@ -61,3 +61,79 @@ function arrayValuesTypes(arr) {
 	});
 	return result;
 }
+
+/* sum of even pairs in an array.
+   Given an array with an even amount of numbers, return true if the sum of two numbers in the array is even and false if the sum of two numbers in the array is odd.
+
+To illustrate:
+
+11, 15, 6, 8, 9, 10
+11 + 15 = 26 = true
+15 + 6 = 21 = false
+6 + 8 = 14 = true
+8+ 9 = 17 = false
+9 + 10 = 19 = false
+Therefore, solution = [true, false, true, false, false]
+
+*/
+function oddSum(numbers) {
+	let count = 0;
+	let splice
+	let result = numbers.map(num => {
+		count++;
+		let add;
+		if(count < numbers.length){
+		     add = num + numbers[count];	
+			   if(add%2 == 0){
+					 return true;
+				 } else {
+					 return false;
+				 }
+		} else {
+			return;
+		}
+	});
+	result.pop();
+	return result;
+}
+
+/*
+Create a function that adds a string ending to each member in an array.
+
+Examples
+addEnding(["clever", "meek", "hurried", "nice"], "ly")
+➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+addEnding(["new", "pander", "scoop"], "er")
+➞ ["newer", "panderer", "scooper"]
+
+addEnding(["bend", "sharpen", "mean"], "ing")
+➞ ["bending", "sharpening", "meaning"]
+
+*/
+
+function addEnding(arr, ending) {
+	let result = arr.map(val => {
+		return val + ending;
+	});
+	return result;
+}
+
+/*
+A number added with its additive inverse equals zero. Create a function that returns an array of additive inverses.
+
+Examples
+additiveInverse([5, -7, 8, 3]) ➞ [-5, 7, -8, -3]
+
+additiveInverse([1, 1, 1, 1, 1]) ➞ [-1, -1, -1, -1, -1]
+
+additiveInverse([-5, -25, 35]) ➞ [5, 25, -35]
+
+*/
+
+function additiveInverse(arr) {
+	let result = arr.map(x => {
+		return x*-1;
+	});
+	return result;
+}
